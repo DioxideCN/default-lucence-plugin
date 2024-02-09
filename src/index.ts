525a5e6f4d5c1b6df453ee5700bda6a67c67b900
Dioxide_CN <dioxide-cn@qq.com>
2024-02-09 12:39:18 +0800

    update default index

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,36 @@
 import { AbstractPlugin } from "./libs/AbstractPlugin";
-import { RendererContext } from "./libs/Plugin";
+import { PluginCommand, PluginRenderers, PluginToolbar, RendererContext } from "./libs/Plugin";
 
 export default class ThemeGravityDom extends AbstractPlugin {
     
     public readonly detail = {
-        icon: "https://dioxide-cn.ink/upload/gravity-logo.png",
-        name: "theme_gravity_renderer",
-        display: "Gravity主题渲染",
-        author: "DioxideCN",
-        version: "1.0.0",
-        description: "嵌入Gravity主题的自定义DOM的显示支持",
-        github: "https://github.com/DioxideCN/plugin-lucence-for-halo",
+        icon: 'https://host/icon.png',
+        name: 'plugin_name',
+        display: 'PluginName',
+        author: 'Author',
+        version: '1.0.0',
+        description: 'Your plugin description',
+        github: 'https://github.com/your_github_repository',
     };
     
-    onEnable() {
-        console.log("'Theme Gravity Custom DOM' extension has been loaded successfully...");
+    public onEnable(): void {
+        console.log('Extension has been loaded...');
     }
 
-    onDisable() {
+    public onDisable(): void {
+        console.log('Extension has been disabled...');
     }
 
-    createRenderer() {
-        return [
-            {
-                name: 'gravity',
-                desc: "渲染Gravity主题的自定义Dom",
-                components: this.createRenderRule(),
-            },
-        ];
+    public createToolbar(): PluginToolbar | null {
+        return null;
     }
-    
-    createRenderRule() {
-        return {
-            big: {
-                desc: '测试',
-                allowContent: true, // 默认为false
-                allowContentHTML: true, // 默认为false，是否允许content被解析为html
-                attributes: [ // 定义属性表单
-                    {
-                        $formkit: 'text',
-                        name: 'owner',
-                        label: '仓库所有者',
-                        help: '填写该Github代码仓库的所有者',
-                        validation: 'required',
-                    },
-                    {
-                        $formkit: 'url',
-                        name: 'repository',
-                        label: '仓库名',
-                        help: '填写该Github代码仓库的仓库名',
-                        validation: 'required|url',
-                    },
-                ],
-                render: function(context: RendererContext) {
-                    const fragment = document.createDocumentFragment();
-                    // 获取内插文本
-                    const content = context.getContent();
-                    // ------ build fragment
-                    const bigDom = document.createElement('big');
-                    bigDom.dataset.owner = context.get('owner', 'default');
-                    bigDom.dataset.repository = context.get('repository', '');
-                    bigDom.innerHTML = content;
-                    fragment.append(bigDom);
-                    return fragment;
-                }
-            },
-        };
+
+    public createCommands(): PluginCommand[] | null {
+        return null;
+    }
+
+    public createRenderer(): PluginRenderers | null {
+        return null;
     }
 
 }
